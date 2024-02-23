2aee4a5945355b730c0c54e2a8f5e8083ae3bffd
John Lee <john.lee24h@gmail.com>
2024-02-23 19:26:06 +0900

    Updated data

--- src/server/initialData.ts
+++ src/server/initialData.ts
@@ -9,32 +9,21 @@ export type Reviews = Array<Review>
 export const initialData: Reviews = [
   {
     id: 0,
-    title: '터미널',
-    comment: '슬픈 영화인 줄 알았는데 코미디!',
-    score: 4,
+    title: 'The Wailing',
+    comment: 'Best Korean horror movie...!',
+    score: 5,
   },
   {
     id: 1,
-    title: '사랑에 대한 모든 것',
-    comment: '스티븐 호킹의 감동 실화ㅠㅠ',
+    title: 'Joker',
+    comment: 'No wonder why Joaquin won Oscar!',
     score: 5,
   },
   {
     id: 2,
-    title: '터미네이터, 다크 페이트',
-    comment: '내 취향은 아니었던 걸로...',
-    score: 2,
-  },
-  {
-    id: 3,
-    title: '날씨의 아이',
-    comment: '그럭저럭 볼만했던 영화 :)',
-    score: 3,
-  },
-  {
-    id: 4,
-    title: '미나리',
-    comment: '윤여정 선생님의 연기가 너무 멋졌다.',
+    title: 'Interstellar',
+    comment:
+      'I had to watch twice to understand fully, but it is one of the best movies in my life.',
     score: 5,
   },
 ]
